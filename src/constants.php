31db15b9ed45d9db45c6ec296eafc06cce2af4d6
mike-c <mike.cokingtin@gmail.com>
2017-12-10 20:30:46 -0500

    testing 1..2..3

---
+++ src/constants.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike.cokingtin
+ * Date: 12/10/17
+ * Time: 8:19 PM
+ */
+
+define('BTX_COMMISSION_PERCENTAGE', .25);
+define('FILL_TYPE_FILL', 1);
+define('FILL_TYPE_PARTIAL', 2);
+define('ORDER_TYPE_BUY', 1);
+define('ORDER_TYPE_SELL',  2);
