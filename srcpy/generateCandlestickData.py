f62d6bece5ae87b7542c1c4725ade83cebe9d454
persinac <persingeralex@yahoo.com>
2017-12-12 13:02:14 -0500

    Committing first set of python files

---
+++ srcpy/generateCandlestickData.py
@@ -0,0 +1,76 @@
+import time
+import numpy
+import talib
+import psycopg2
+from datetime import datetime
+from pprint import pprint
+from datetime import timedelta
+
+def reject_outliers(data, m=2):
+    return data[abs(data - numpy.mean(data)) < m * numpy.std(data)]
+
+try:
+    connect_str = "dbname='btx' user='btx_user' host='localhost' password='btx_server_112817'"
+    # use our connection values to establish a connection
+    conn = psycopg2.connect(connect_str)
+    # create a psycopg2 cursor that can execute queries
+    # cursor = conn.cursor()
+    # cursor.execute("""SELECT * from btxcoinheader""")
+    # rows = cursor.fetchall()
+    # print(rows)
+    # print(datetime.datetime.now())
+    epoch_time = int(time.time())
+
+    currentTime = datetime.now()
+    # Actually going to calculate 1 minute behind - to allow the
+    # other batch jobs to complete / insert data
+    previousMinuteTime_nonFormat = currentTime - timedelta(minutes=2)
+    previousMinuteReplaceSeconds = previousMinuteTime_nonFormat.replace(second=0, microsecond=0)
+
+    currMinuteTime_nonFormat = currentTime - timedelta(minutes=1)
+    currMinuteTimeReplaceSeconds = currMinuteTime_nonFormat.replace(second=0, microsecond=0)
+
+    # convert the times to EPOCH
+    epochPrevTime = int(previousMinuteReplaceSeconds.timestamp())
+    epochCurrTime = int(currMinuteTimeReplaceSeconds.timestamp())
+    print(str(previousMinuteReplaceSeconds) + " | " + str(epochPrevTime)
+          + " Curr: " + str(currMinuteTimeReplaceSeconds) + " | " + str(epochCurrTime))
+
+
+    ##
+    # when accessing the rows, use the following Index values that correspond to columns:
+    # 0 - id
+    # 1 - btxid
+    # 2 - quantity
+    # 3 - value
+    # 4 - total
+    # 5 - btxtimestamp
+    # 6 - usdValue
+    #
+    # numpy.argmin() will give us the index
+    #
+    # test empty result set with btxtimestamp low = 1513039680 / high = 1513039740
+    #
+    #
+    ##
+    sqlSelect = "SELECT id, btxid, quantity, value, total, btxtimestamp, \"usdValue\" from btxcoinmarkethistorydetails"
+    sqlWhere = "WHERE coin = '%s' AND btxtimestamp >= %d AND btxtimestamp < %d " % ("ETH", epochPrevTime, epochCurrTime)
+    sqlOrderBy = "ORDER BY btxtimestamp ASC" #the first row is going to be the opener / last = closer
+    sqlStmnt = sqlSelect + " " + sqlWhere + " " + sqlOrderBy
+    cursor = conn.cursor()
+    cursor.execute(sqlStmnt)
+    rows = cursor.fetchall()
+    ## Open-High-Low-Close Candlestick(s)
+    opener = rows[0][3]
+    closer = rows[len(rows)-1][3]
+    ##this inline for each thing is known as: List Comprehensions
+    x = numpy.array([row[3] for row in rows])
+    #filter any outliers
+    filtered = numpy.array(reject_outliers(x))
+    print(opener)
+    print(closer)
+    print(filtered.min())
+    print(filtered.max())
+except Exception as e:
+    print("Uh oh, can't connect. Invalid dbname, user or password?")
+    print(e)
---
+++ srcpy/getMarketHistoryDetails.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+import sys
+import logging
+sys.path.append('/var/www/html/python_bittrex_master/bittrex')
+
+logging.basicConfig(filename='/var/www/html/example.log',level=logging.DEBUG)
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+
+
+class reg(object):
+	def __init__(self, cursor, registro):
+		for (attr, val) in zip((d[0] for d in cursor.description), registro) :
+			setattr(self, attr, val)
+
+config = {
+	'user': 'bittrex_user',
+	'password': 'password123',
+	'host': 'localhost',
+	'database': 'bittrex'
+}
+
+market = 'BTC'
+coinsToWatch = ['EMC2', 'BAT', 'XRP', 'MONA', 'ETH','NEO','OMG','LTC']
+
+try:
+    from bittrex import Bittrex
+    import psycopg2
+    from datetime import datetime
+    import jsonpickle
+    from pprint import pprint
+    from datetime import timedelta
+
+    connect_str = "dbname='bittrex' user='bittrex_user' host='localhost' password='password123'"
+    conn = psycopg2.connect(connect_str)
+    cursor = conn.cursor()
+    cursor.execute("select * from btxmarkethistory")
+    rows = cursor.fetchall()
+    colnames = [desc[0] for desc in cursor.description]
+    # print(colnames)
+
+    #set API key
+    bittrex = Bittrex('e281fee0429e428b9a76f63cc842f12d', 'a131b56ec7f24696a74c6b6da853d1eb')
+
+    #get balance - just to test integration
+    actual = bittrex.get_balance('BTC')
+
+    dataTuple = []
+    for value in coinsToWatch:
+        ## get various market summaries
+        specificMarketHistory = bittrex.get_market_history((market + '-' + str(value)), 30)
+        # print(str(marketSummary['result']))
+        if specificMarketHistory['success'] == True:
+            for valuetwo in specificMarketHistory['result']:
+                currentTime = datetime.now()
+                previousMinuteTime_nonFormat = currentTime - timedelta(minutes=1)
+                previousMinuteReplaceSeconds = previousMinuteTime_nonFormat.replace(second=0, microsecond=0)
+
+                currMinuteTime_nonFormat = currentTime
+                currMinuteTimeReplaceSeconds = currMinuteTime_nonFormat.replace(second=0, microsecond=0)
+
+                strippedMSIdx = str(valuetwo['TimeStamp']).find(".")
+                parsedTime = ""
+                if strippedMSIdx > 0:
+                    substrTime = str(valuetwo['TimeStamp'])[:strippedMSIdx]
+                    parsedTime = datetime.strptime(substrTime, "%Y-%m-%dT%H:%M:%S")
+                else:
+                    parsedTime = datetime.strptime(substrTime, "%Y-%m-%dT%H:%M:%S")
+                compareLow = parsedTime >= previousMinuteReplaceSeconds
+                compareHigh = parsedTime < currMinuteTimeReplaceSeconds
+                # print(str(parsedTime) + " >= " + str(previousMinuteReplaceSeconds) +": "+ str(compareLow))
+                # print(str(parsedTime) + " < " + str(currMinuteTimeReplaceSeconds) + ": " + str(compareHigh))
+
+                # another way to compare this so that we don't get duplicates is
+                # to query the DB for all btxids and only insert
+                # the values that don't already exist in our DB
+                # for now this should be fine...check for duplicates later
+                if compareLow == True:
+                    if compareHigh == True:
+                        ###
+                        # btxid, coin, market,
+                        # quantity, value, total,
+                        # filltype, ordertype, btxtimestamp
+                        ###
+                        mytuple = (valuetwo['Id'], value, market,
+                        valuetwo['Quantity'], valuetwo['Price'],
+                         valuetwo['Total'], valuetwo['FillType'],
+                         valuetwo['OrderType'], valuetwo['TimeStamp'])
+                        dataTuple.append(mytuple)
+
+
+    rowsToInsert = ",".join(
+        cursor.mogrify('(%s,%s,%s,%s,%s,%s,%s,%s,%s)', thing).decode('UTF-8')
+        for thing in dataTuple
+    )
+    # print(str(rowsToInsert))
+    cursor.execute("""INSERT INTO btxcoinmarkethistorydetails(
+    btxid, coin, market, quantity, value, total, filltype, ordertype, btxtimestamp) VALUES """ + rowsToInsert)
+    conn.commit()
+
+except:
+    logger.exception('Main Exception handler')
+    raise
