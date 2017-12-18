7529303b120205c2a399af5148bd0ae8137433ed
persinac <persingeralex@yahoo.com>
2017-12-18 16:24:10 -0500

    Updated all calculation methods to accept params.

--- srcpy/calculateRSI.py
+++ srcpy/calculateRSI.py
@@ -29,12 +29,15 @@ try:
         , user=params['pgsql']['user']
         , password=params['pgsql']['password']
     )
-    currCoin = 'POT'
-    currMarket = 'BTC'
+    currMarket = sys.argv[1]
+    currCoin = sys.argv[2]
+    limit = int(sys.argv[3])
+    interval = int(sys.argv[4])
+    timePeriod = int(sys.argv[5])
     sqlSelect = "SELECT id, closer from tkmcandlesticks"
     sqlWhere = "WHERE market = '%s' AND coin = '%s'" % (currMarket, currCoin)
     sqlOrder = "ORDER BY timestampintervallow DESC"
-    sqlLimit = "LIMIT 500"
+    sqlLimit = "LIMIT %s" % (limit)
     sqlStmnt = sqlSelect + " " + sqlWhere + " " + sqlOrder + " " + sqlLimit
     cursor = conn.cursor()
     cursor.execute(sqlStmnt)
@@ -42,15 +45,10 @@ try:
     if len(rows) > 0:
         initialData = rows[::-1]
         close = numpy.array([item[1] for item in initialData])
-        fiveMinuteCalc = numpy.array([close[j] for j in range(len(close)) if j % 5 == 0])
-        outputRSI = talib.RSI(fiveMinuteCalc, 14)
-        outputSMA = talib.SMA(fiveMinuteCalc, 14)
-        cleanedList = [[j, outputRSI[j], outputSMA[j], str("RSI: %s | SMA: %s"%(outputRSI[j],outputSMA[j]))] for j in range(len(outputRSI)) if not math.isnan(outputRSI[j])]
+        # fiveMinuteCalc = numpy.array([close[j] for j in range(len(close)) if j % 5 == 0])
+        outputRSI = talib.RSI(close, timePeriod)
+        cleanedList = [[j, outputRSI[j], str("RSI: %s"%(outputRSI[j]))] for j in range(len(outputRSI)) if not math.isnan(outputRSI[j])]
         print(cleanedList)
-
-        # print(sqlStmnt)
-    # print ('Number of arguments: %s args' % (len(sys.argv)))
-    # print ('Argument List:' + str(sys.argv))
 
 except Exception as e:
     print("Uh oh, you done fucked up: %s" % (e))
